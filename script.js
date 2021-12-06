1b987e6cd9d0f84e93087a3ebf30b7760c1ead00
spell-landon <spell.landon@gmail.com>
2021-12-05 18:32:56 -0600

    Add Modal Input Constants to .js

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-// grab the add new item button
+//? Grab the add new item button
 const newItem = document.querySelector('#add-new-item');
 const modalEl = document.querySelector('#modal');
 const closeNewItem = document.querySelector('.close');
 const createJobSearchBtn = document.querySelector('#create-button-modal');
 const formEl = document.querySelector('.job-inputs');
-// Table elements
-const tableDate = document.querySelector('#td-date');
+//? Modal Input Variables
+const modalInputDate = document.querySelector('#input-date');
+const modalInputTime = document.querySelector('#input-time');
+const modalInputCompany = document.querySelector('#input-company');
+const modalInputInterviewer = document.querySelector('#input-interviewer');
+const modalInputJobTitle = document.querySelector('#input-job-title');
+const modalInputJobDescription = document.querySelector(
+  '#input-job-description'
+);
+const modalInputSalary = document.querySelector('#input-salary');
+const modalInputRemote = document.querySelector('#input-remote');
+const modalInputSecondInterview = document.querySelector(
+  '#input-second-interview'
+);
 
+//? Create new element variables
+const createTr = document.createElement('tr');
+const createTd = document.createElement('td');
 
-// Event Handlers
+//? Event Handlers
+// Modal Open
 const openAddNewItemModal = () => {
-  // modalEl.style.display = 'block'
   modalEl.classList.remove('show-modal');
   modalEl.classList.add('show-modal');
 };
+// Modal Close
 const closeNewJobModal = () => {
-  //   modalEl.style.display = 'none';
   modalEl.classList.remove('show-modal');
 };
+// Create new Table row and add content
 
-// Event Listeners
+//? Event Listeners
 newItem.addEventListener('click', openAddNewItemModal); // Opens the modal when clicked
 closeNewItem.addEventListener('click', closeNewJobModal); // Closes the modal when the 'X' is clicked
 createJobSearchBtn.addEventListener('click', closeNewJobModal); // Closes the modal when the 'Create New Job' button is clicked
