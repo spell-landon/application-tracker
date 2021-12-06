44a7a4c2333a48239d305c94eece4eff5ec655f6
spell-landon <spell.landon@gmail.com>
2021-12-05 18:25:22 -0600

    Add table to html. Starting on .js

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ const newItem = document.querySelector('#add-new-item');
 const modalEl = document.querySelector('#modal');
 const closeNewItem = document.querySelector('.close');
 const createJobSearchBtn = document.querySelector('#create-button-modal');
+const formEl = document.querySelector('.job-inputs');
+// Table elements
+const tableDate = document.querySelector('#td-date');
+
 
 // Event Handlers
-const addNewJob = () => {
+const openAddNewItemModal = () => {
   // modalEl.style.display = 'block'
   modalEl.classList.remove('show-modal');
   modalEl.classList.add('show-modal');
 };
-const closeNewJob = () => {
+const closeNewJobModal = () => {
   //   modalEl.style.display = 'none';
   modalEl.classList.remove('show-modal');
 };
 
 // Event Listeners
-newItem.addEventListener('click', addNewJob);
-closeNewItem.addEventListener('click', closeNewJob);
-// modalEl.addEventListener('click', closeNewJob);
-createJobSearchBtn.addEventListener('click', closeNewJob);
+newItem.addEventListener('click', openAddNewItemModal); // Opens the modal when clicked
+closeNewItem.addEventListener('click', closeNewJobModal); // Closes the modal when the 'X' is clicked
+createJobSearchBtn.addEventListener('click', closeNewJobModal); // Closes the modal when the 'Create New Job' button is clicked
+formEl.addEventListener('submit', closeNewJobModal); // Submits the form from the modal when every input is filled out and the 'Create' button is submitted with the "Enter" key from keyboard
